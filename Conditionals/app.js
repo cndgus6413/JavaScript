917d9d939f8405bb982fb3784cdef6f97785c8b8
cndgus6413 <cndgus6413@naver.com>
2022-06-29 18:22:41 +0900

    loops

--- Conditionals/app.js
+++ Conditionals/app.js
@@ -74,3 +74,13 @@ switch (day) {
         console.log("Weekend");
         break;
 }
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+let j = 0;
+while (j < 10) {
+    console.log(j);
+    j += 1
+}
